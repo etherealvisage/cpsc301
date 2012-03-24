9ba3de9ef197aa9ad1275e5c532812cbabbd3c41
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-03-24 17:11:14 -0600

    Fix broken URL routing.

--- webclient/js/init.js
+++ webclient/js/init.js
@@ -5,10 +5,14 @@
 
   // Prevent browser from navigating to new page when link activated.
   $('body').on('click', 'a', function(evt) {
-    if(window.location.host !== evt.target.host)
+    if(window.location.host !== evt.currentTarget.host)
       return true;
     evt.preventDefault();
-    router.navigate('/discussions', true);
+
+    // If we use the query string at all, we will also need to reference
+    // evt.currentTarget.search. As long as we stick to "pretty" URLs, however,
+    // this should not be a problem.
+    router.navigate(evt.currentTarget.pathname, true);
   });
 
   Backbone.history.start({pushState: true});
