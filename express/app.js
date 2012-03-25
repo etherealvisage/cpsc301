9e0ef20ac8c9796da3abeae07f79512a06fb2d97
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-03-25 16:00:59 -0600

    Clean up directory structure.

--- express/app.js
+++ express/app.js
@@ -5,12 +5,11 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-// Configuration
 
+/*=============
+  Configuration
+  =============*/
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -26,11 +25,17 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
 
+/*======
+  Routes
+  ======*/
 app.get('/', routes.index);
 app.get('/discussions', routes.listDiscussions);
 app.post('/discussions', routes.createDiscussion);
 
+
+/*=====================
+  Server initialization
+  =====================*/
 app.listen(config.serverPort);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
