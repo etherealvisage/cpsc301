bc99107d1c1427fc78fb8662f50f3d558208816c
ethereal <ethereal.visage@gmail.com>
2012-03-28 10:16:23 -0600

    Implemented some of the frontend login handling code; a request is now sent.

--- webclient/js/auth.js
+++ webclient/js/auth.js
@@ -14,6 +14,23 @@ Authentication.LoginView = Backbone.View.extend({
   render: function() {
     $(this.el).empty();
     $(this.el).html(this.template);
+
+    /* Initialize page elements. */
+    var self = this;
+    console.log($("#authentication-login-submit"));
+    $("#authentication-login-submit").click(function() {
+      $.ajax({
+        url: "/api/authenticate",
+        type: "POST",
+        data: {
+          username: $("#authentication-login-username").val(),
+          password: $("#authentication-login-password").val()
+        }
+      }).done(function() { 
+        console.log("!");
+      });
+      return false;
+    });
   }
 });
 
