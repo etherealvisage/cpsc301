d23057d080aa7983c9e1cdbbbc64020f23d02996
Michael Wing <mvwing@gmail.com>
2012-04-02 15:40:54 -0600

    Just added token checking to the existing discussion functions, identical to the memo's token checking
    ~

--- server/src/routes/discussions.js
+++ server/src/routes/discussions.js
@@ -1,22 +1,28 @@
 var models = require('../models');
 
 exports.listDiscussions = function(req, res) {
-  var discussion = new models.DiscussionRegistry();
-  discussion.list(function(rows) {
-    res.json(rows);
+  util.checkToken(req, res, function() {  
+    var discussion = new models.DiscussionRegistry();
+    discussion.list(function(rows) {
+      res.json(rows);
+    });
   });
 };
 
 exports.createDiscussion = function(req, res) {
-  var discussion = new models.DiscussionRegistry();
-  discussion.create(req.body);
-  res.end();
+  util.checkToken(req, res, function() {    
+    var discussion = new models.DiscussionRegistry();
+    discussion.create(req.body);
+    res.end();
+  });
 };
 
 exports.getDiscussion = function(req, res) {
-  var discussion_id = parseInt(req.params.id, 10);
-  var discussion = new models.Discussion;
-  discussion.load(discussion_id, function(result) {
-    res.json(result);
+  util.checkToken(req, res, function() {  
+    var discussion_id = parseInt(req.params.id, 10);
+    var discussion = new models.Discussion;
+    discussion.load(discussion_id, function(result) {
+      res.json(result);
+    });
   });
 };
