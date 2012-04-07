814b5caf13254d2de5c8fa9897cf3f7555ec0081
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-04-07 17:52:55 -0600

    Move user-related routes to appropriate module.

--- server/src/routes/authenticate.js
+++ server/src/routes/authenticate.js
@@ -1,4 +1,3 @@
-var crypto = require('crypto');
 var models = require('../models');
 var util = require("./util");
 
@@ -26,25 +25,4 @@ exports.logout = function(req, res) {
     authenticator.logout(session);
   });
   res.json({});
-}
-
-exports.createUser = function(req, res) {
-  util.checkTokenAndPermission(req, res, "createUser", function() {
-    var authenticator = new models.Authentication();
-    var username = req.body.username;
-    var name = req.body.name;
-    var password = req.body.password;
-    var userType = req.body.userType;
-    
-    authenticator.createUser(username, name, password, userType, function(data) {
-      res.json({uid: data.uid});
-    });
-  });  
-}
-
-exports.listUsers = function(req, res) {
-  var authenticator = new models.Authentication();
-  authenticator.listUsers(function(data) {
-    res.json(data);
-  });
 }
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,3 +9,24 @@ exports.getUser = function(req, res) {
     });
   });
 }
+
+exports.createUser = function(req, res) {
+  util.checkTokenAndPermission(req, res, "createUser", function() {
+    var authenticator = new models.Authentication();
+    var username = req.body.username;
+    var name = req.body.name;
+    var password = req.body.password;
+    var userType = req.body.userType;
+
+    authenticator.createUser(username, name, password, userType, function(data) {
+      res.json({uid: data.uid});
+    });
+  });
+}
+
+exports.listUsers = function(req, res) {
+  var authenticator = new models.Authentication();
+  authenticator.listUsers(function(data) {
+    res.json(data);
+  });
+}
