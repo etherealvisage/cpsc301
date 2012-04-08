860ca250aead5a22a1b75e31f61bbfe4efdf89c6
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-04-08 17:10:08 -0600

    Tweak memo test.

--- server/test/memoTest.js
+++ server/test/memoTest.js
@@ -7,10 +7,7 @@ describe('Memos', function() {
     fixtures.load('users', 'sessions', 'memos', done); 
   });
 	
-
-
   //List all memos
-  //	
   describe('Listing', function() {
     it('should return the correct MIME type', function(done) {
       testHelpers.makeGetReq('/api/memos', function(res) {
@@ -21,95 +18,48 @@ describe('Memos', function() {
 
     it('should list all memos in DB', function(done) {
       testHelpers.makeGetReq('/api/memos', function(res) { 
-	//console.log(res);
       }, function(body) {
-        var expected = [{ 
+        var expected = [{
           "id": 1,
-          "title": "test memo",
-          "posterID": 2,
-          "postDate": 1333819009,
-          "content": "this is a test body",
-          "posterName": "Margot",
-          "unread": 0 
-          },
-          {
-          "id": 2,
-          "title": "another memo",
-          "posterID": 3,
-          "postDate": 1333911831,
-          "content": "Hello program",
-          "posterName": "Dr. Banana",
-          "unread": 1 
+          "title": "Ooh, a title!",
+          "posterID": 1,
+          "postDate": 1333820201,
+          "content": "I am a memo.",
+          "posterName": "mike",
+          "unread": 0
         }];
         var actual = JSON.parse(body);
-	//console.log('\n The expected body recieved \n');
-	//console.log(expected);
-	//console.log('\n And now the actual body recieved \n');
-	//console.log(actual);
-        assert.deepEqual(expected, actual, 'Memos do not match');
+        assert.deepEqual(expected, actual);
         done();
       });
     });
-
-
   });
 
 
-  //loading single memos
   describe('Single Memos', function() {
-    
     it('should list memo 1', function(done) {
-      testHelpers.makeGetReq('/api/memos/1', function(res) { 
-	//console.log(res);
+      testHelpers.makeGetReq('/api/memos/1', function(res) {
       }, function(body) {
-        var expected = { 
+        var expected = {
           "id": 1,
-          "title": "test memo",
-          "posterID": 2,
-          "postDate": 1333819009,
-          "content": "this is a test body" 
+          "title": "Ooh, a title!",
+          "posterID": 1,
+          "postDate": 1333820201,
+          "content": "I am a memo."
         };
+
         var actual = JSON.parse(body);
-	//console.log('\n The expected body recieved \n');
-	//console.log(expected);
-	//console.log('\n And now the actual body recieved \n');
-	//console.log(actual);
-        assert.deepEqual(expected, actual, 'Memos do not match');
+        assert.deepEqual(expected, actual);
         done();
       });
     });
 
-
-
-    it('should fail to list the non-existant memo5', function(done) {
-      testHelpers.makeGetReq('/api/memos/5', function(res) { 
-	//console.log(res);
+    /*it('should fail to list the non-existent memo', function(done) {
+      testHelpers.makeGetReq('/api/memos/1', function(res) { 
       }, function(body) {
-        var actual = null;
-	try{
-          console.log("trying"); 
-          actual = JSON.parse(body);
-	} catch (err) {
-	  console.log("exception caught");
-	  console.log(err);
-	  done();
-	}
-	
-	//console.log('\n The expected body recieved \n');
-	//console.log('\n And now the actual body recieved \n');
-	//console.log(actual);
-       // done();
+        done();
       });
-    });
-
-
-
-
-
-
+    });*/
   });
-
 		/////////////for create with the post request, use the info here https://gist.github.com/7f4ba4fbad8a63a86183
-
-
 });
