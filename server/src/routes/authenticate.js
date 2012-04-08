f0b63b9c3c590791491cd78fb13b43885aeca04c
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-04-07 18:49:39 -0600

    Add 'remember me' to login form.

--- server/src/routes/authenticate.js
+++ server/src/routes/authenticate.js
@@ -4,16 +4,16 @@ var util = require("./util");
 exports.login = function(req, res) {
   var authenticator = new models.Authentication();
   authenticator.login(req.body.username, req.body.password, function(result) {
-    if(result.state === 'success')
-      res.cookie('session', result.token, {
-        httpOnly: true,
-        maxAge: 14*24*60*60*1000, // 2 weeks
-      });
-      /* NOTE: perhaps find a better way than creating a new cookie? */
-      res.cookie('uid', result.userID, {
-        httpOnly: true,
-        maxAge: 14*24*60*60*1000, // 2 weeks
-      });
+
+    if(result.state === 'success') {
+      var cookie_params = { httpOnly: true };
+      if(req.body.remember_me === 'on')
+        cookie_params.maxAge = 14*24*60*60*1000; // 2 weeks
+
+      res.cookie('session', result.token, cookie_params);
+      res.cookie('uid', result.userID, cookie_params);
+    }
+
     res.json(result);
   });  
 };
