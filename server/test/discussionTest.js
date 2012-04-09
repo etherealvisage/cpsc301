1f46182164e56a269f17a470bc07af243aba237c
Stephen Huang <stephenhuang1@gmail.com>
2012-04-09 10:14:13 -0600

    Updated discussion Tests

--- server/test/discussionTest.js
+++ server/test/discussionTest.js
@@ -56,23 +56,42 @@ describe('Discussions', function() {
         }, 
         function(body) {
           var expected = { posts: 
-   [ { id: 1,
-       posterID: 2,
-       postDate: 1333740076,
-       body: 'This is my body. There are many like it, but this is mine.',
-       discussionID: 1,
-       authorName: 'Margot & the Nuclear So-Sos' } ],
-  title: 'title' };
-	  var actual = JSON.parse(body);
-	  console.log('\n The expected body recieved \n');
-	  console.log(expected);
+          [ { id: 1,
+             posterID: 2,
+             postDate: 1333740076,
+             body: 'This is my body. There are many like it, but this is mine.',
+             discussionID: 1,
+             authorName: 'Margot & the Nuclear So-Sos' } ],
+             title: 'title' };
+	         var actual = JSON.parse(body);
+	         console.log('\n The expected body recieved \n');
+	         console.log(expected);
 
-	  console.log('\n And now the actual body recieved \n');
-	  console.log(actual);
-          assert.deepEqual(expected, actual, 'Discussions do not match');
-          done();
-	}
+	         console.log('\n And now the actual body recieved \n');
+	         console.log(actual);
+           assert.deepEqual(expected, actual, 'Discussions do not match');
+           done();
+  	  }
       );
     });
   });
+  
+  describe('Create Discussion', function() {
+     it('should succesfully create a discussion', function(done){ 
+         testHelpers.makePostReq('/api/discussions', {
+         title: 'LOL this is a title',
+         content: 'And this is the corresponding body.'
+       }, function(res) {
+           console.log(res.statusCode);
+           assert.equal(res.statusCode,200);
+       }, function(body) {
+           console.log(" THIS IS THE BODY", body);         
+           done();
+     });
+  	 });
+    });
 });
+
+
+  
+
